a28f3ca82ecb7f4cd4f111e31ce854a7218cd0b2
simonecapezzani <simonecapezzani@gmail.com>
2024-05-31 10:48:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+createBrowserRouter(
+  //Pass an array of route definition objects
+  [
+    {}
+  ]
+)
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
