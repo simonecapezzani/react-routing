2b6c997a5cb29599284f832df1b8d64b94e02327
simonecapezzani <simonecapezzani@gmail.com>
2024-05-31 15:39:45 +0200

    Add second route and implemented Link

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { createBrowserRouter , RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
 
 const router = createBrowserRouter(
   //Pass an array of route definition objects
   [
-    {path: '/', element: <HomePage/> }
+    { path: '/', element: <HomePage /> },
+    { path: '/products', element: <ProductsPage /> }
   ]
-)
+);
+
+// Alternative routes definition
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage />} />
+//     <Route path='/products' element={<ProductsPage />} />
+//   </Route>
+// );
 
 
 function App() {
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,12 @@
-function HomePage(){
-    return <h1>My Home Page</h1>;
+import { Link } from "react-router-dom";
+
+// Using Link instead of "a" prevents the default event and changes the URL
+// without sending the HTTP request
+function HomePage() {
+    return <>
+        <h1>My Home Page</h1>
+        <p>Go to <Link to="/products">the products list</Link></p>
+    </>
 }
 
 export default HomePage;
