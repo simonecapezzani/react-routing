efa990df853ba474b89f1f688105d63265637acd
simonecapezzani <simonecapezzani@gmail.com>
2024-06-03 09:39:42 +0200

    Navigate programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 // Using Link instead of "a" prevents the default event and changes the URL
 // without sending the HTTP request
 function HomePage() {
+
+    const navigate = useNavigate();
+
+    function navigateHandler(){
+        navigate('/products');
+    }
+
     return <>
         <h1>My Home Page</h1>
         <p>Go to <Link to="/products">the products list</Link></p>
+        <p><button onClick={navigateHandler}>Navigate</button></p>
     </>
 }
 
