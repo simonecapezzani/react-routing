7846104dffb450823a1f34b197d376476d704c72
simonecapezzani <simonecapezzani@gmail.com>
2024-06-03 15:44:09 +0200

    Dynamic routing with parameters

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 import ErrorPage from './pages/Error';
+import ProductDetailPage from './pages/ProductDetail';
 
 const router = createBrowserRouter(
   //Pass an array of route definition objects
@@ -10,7 +11,9 @@ const router = createBrowserRouter(
     {
       path: '/', element: <RootLayout />, children: [
         { path: '/', element: <HomePage /> },
-        { path: '/products', element: <ProductsPage />}
+        { path: '/products', element: <ProductsPage />},
+        // to pass dynamic parameters, we use the ":" followed by the parameter name
+        { path: '/products/:productId', element: <ProductDetailPage />},
       ], errorElement: <ErrorPage />
     },
 
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,21 @@
-function ProductsPage(){
-    return <h1>The Products Page</h1>;
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+    {id: 'p1',title: 'Product 1'},
+    {id: 'p2',title: 'Product 2'},
+    {id: 'p3',title: 'Product 3'},
+    {id: 'p4',title: 'Product 4'},
+]
+
+function ProductsPage() {
+    return <>
+        <h1>The Products Page</h1>
+        <ul>
+            {PRODUCTS.map((prod)=>{
+                return <li><Link id={prod.id} key={prod.id} to={`/products/${prod.id}`}>{prod.title}</Link></li>
+            })}
+        </ul>
+    </>;
 }
 
 export default ProductsPage;
